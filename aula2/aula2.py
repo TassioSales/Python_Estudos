15e73be1f057643923a5b39c03ecf63e57b3c2f0
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-09-03 00:46:34 -0300

    commit inicial

---
+++ aula2/aula2.py
@@ -0,0 +1,13 @@
+print(123456)
+print("Luis", "Otavio", "Outra coisa")
+print("Luis", "Otavio", sep='_', end="#####")
+print("João", "e", "Maria\n", sep="_")
+
+
+"""
+824.176.070-18
+"""
+
+print("824", "176", "070", sep=".", end = "-")
+
+print("18")
