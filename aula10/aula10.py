54ac50066e64a469fc49bbb0d7800921e61ca0df
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-09-07 23:03:13 -0300

    commit inicial

---
+++ aula10/aula10.py
@@ -0,0 +1,22 @@
+"""
+operadores relacionais
+>,<,==, , >=, <=, !=
+"""
+
+print(2 == 2)
+print(2 >= 2)
+print(2 == 1)
+
+num1 = 2
+num2 = 2
+num3 = 3
+
+print(num1, num2)
+
+print(num1 == num2)
+print(num1 > num2 )
+print(num1 >= num2)
+print(num1 <= num3)
+expressao = num1 == num2
+
+print(expressao)
