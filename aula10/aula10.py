887394ff5929e317405d4ce14a09bb2a912313af
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-09-09 03:47:10 -0300

    commit inicial

--- aula10/aula10.py
+++ aula10/aula10.py
@@ -14,9 +14,18 @@ num3 = 3
 print(num1, num2)
 
 print(num1 == num2)
-print(num1 > num2 )
+print(num1 > num2)
 print(num1 >= num2)
 print(num1 <= num3)
 expressao = num1 == num2
 
 print(expressao)
+nome = input("Qual seu nome ? ")
+idade = input("qual sua idade: ")
+idade_limite = 18
+idade = int(idade)
+
+if idade >= idade_limite:
+    print("Voce pode pegar o emprestimo")
+else:
+    print("Voce ainda nao tem idade suficiente par adquirir um emprestimo ")
