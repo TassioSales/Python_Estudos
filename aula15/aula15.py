88a0079795b2e09588c88a78cd3e7e7eccf3f61d
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-09-11 01:22:46 -0300

    commit inicial

---
+++ aula15/aula15.py
@@ -0,0 +1,14 @@
+"""
+manipulando strigs
+"""
+
+teste = "python S2"
+
+print(teste[2])
+print(teste[8])
+print(teste[-1])
+print(teste[-2])
+print(teste[2:6])
+print(teste[:6])
+print(teste[7:])
+print(teste[0::2])
