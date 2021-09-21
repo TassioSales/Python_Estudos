361f0022d3acbe8e2641c11c3fc7ad2899cc310b
TassioSales <tassio.ljs@gmail.com>
2021-09-21 15:37:48 -0300

    first commit

--- modulo_2/aula_9/aula9.py
+++ modulo_2/aula_9/aula9.py
@@ -25,3 +25,11 @@ print(l7)
 print(l8)
 print(l9)
 print(dict1)
+
+
+l10 = list(range(100))
+print(l10)
+ex6 = [v for v in l10 if v % 3 == 0 if v % 8 == 0]
+ex7 = [v if v % 3 == 0 and v % 8 == 0 else "0" for v in l10]
+print(ex6)
+print(ex7)
