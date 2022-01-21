4878b6f8ded863ed5482cdc0400a3bf7df838455
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-01-20 23:26:51 -0300

    Adicionando Exercicios de POO parte 2

--- Python Orientado Objeto (POO)/encapsulamento/basededados.py
+++ Python Orientado Objeto (POO)/encapsulamento/basededados.py
@@ -1,24 +1,27 @@
 """
     public, protect, private
+    _privado/protect
+    __ privado
 """
 
 
 class BaseDeDados:
     def __init__(self):
-        self.dados = {}
+        self.__dados = {}
+
+    @property
+    def dados(self):
+        return self.__dados
 
     def inserir_cliente(self, id, nome):
-        if "clientes" not in self.dados:
-            self.dados['clientes'] = {id: nome}
+        if "clientes" not in self.__dados:
+            self.__dados['clientes'] = {id: nome}
         else:
-            self.dados['clientes'].update({id: nome})
+            self.__dados['clientes'].update({id: nome})
 
     def lista_clientes(self):
-        for id, nome in self.dados['clientes'].items():
+        for id, nome in self.__dados['clientes'].items():
             print(id, nome)
 
     def apaga_cliente(self, id):
-        del self.dados['clientes'][id]
-
-
-
+        del self.__dados['clientes'][id]
--- Python Orientado Objeto (POO)/encapsulamento/main.py
+++ Python Orientado Objeto (POO)/encapsulamento/main.py
@@ -5,6 +5,10 @@ bd = BaseDeDados()
 bd.inserir_cliente(1, "Tassio")
 bd.inserir_cliente(2, "Lucian")
 bd.inserir_cliente(3, "Jesus")
-bd.apaga_cliente(3)
 bd.lista_clientes()
+
+print(bd.dados)
+
+
+
 
