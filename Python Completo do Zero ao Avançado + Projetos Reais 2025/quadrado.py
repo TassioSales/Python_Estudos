bca6b2419ee8022286f8aec0a999fadbf5e190e8
TassioSales <74218122+TassioSales@users.noreply.github.com>
2025-04-28 13:09:00 -0300

    Create quadrado.py

---
+++ Python Completo do Zero ao Avançado + Projetos Reais 2025/quadrado.py
@@ -0,0 +1,7 @@
+# Exercício 1: Quadrado - Perímetro e Área
+# Recebe o valor do lado de um quadrado e imprime perímetro e área no formato solicitado
+
+lado = int(input('Digite o valor correspondente ao lado de um quadrado: '))
+perimetro = 4 * lado
+area = lado * lado
+print(f'perímetro: {perimetro} - área: {area}')
