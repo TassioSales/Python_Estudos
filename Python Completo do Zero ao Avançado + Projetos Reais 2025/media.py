2541d246726470d57ba4a94c521bf63a95272abf
TassioSales <74218122+TassioSales@users.noreply.github.com>
2025-04-28 13:08:58 -0300

    Create media.py

---
+++ Python Completo do Zero ao Avançado + Projetos Reais 2025/media.py
@@ -0,0 +1,9 @@
+# Exercício 2: Média aritmética de quatro notas
+# Recebe quatro notas e imprime a média aritmética no formato solicitado
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+nota3 = float(input('Digite a terceira nota: '))
+nota4 = float(input('Digite a quarta nota: '))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print(f'A média aritmética é {media}')
