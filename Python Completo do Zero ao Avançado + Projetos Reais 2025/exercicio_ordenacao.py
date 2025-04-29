24ada267ab233d52c11fa0f1c23b7ac81fbfeeea
TassioSales <74218122+TassioSales@users.noreply.github.com>
2025-04-29 01:08:14 -0300

    Create exercicio_ordenacao.py

---
+++ Python Completo do Zero ao Avançado + Projetos Reais 2025/exercicio_ordenacao.py
@@ -0,0 +1,8 @@
+# Exercício 5 - Verificando ordenação
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+c = int(input("Digite o terceiro número: "))
+if a < b < c:
+    print("crescente")
+else:
+    print("não está em ordem crescente")
