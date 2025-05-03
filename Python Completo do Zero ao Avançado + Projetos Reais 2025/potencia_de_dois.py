f0dfe31d8d643b0de74f16ed3dcbbfa7d55105a8
TassioSales <74218122+TassioSales@users.noreply.github.com>
2025-05-03 19:49:37 -0300

    Create potencia_de_dois.py

---
+++ Python Completo do Zero ao Avançado + Projetos Reais 2025/potencia_de_dois.py
@@ -0,0 +1,9 @@
+# arquivo: potencias_de_dois.py
+
+potencia = 0
+valor = 1
+
+while potencia <= 20:  # Altere 10 para o limite desejado
+    print(f"2^{potencia} = {valor}")
+    potencia += 1
+    valor *= 2
