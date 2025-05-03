25da054d23f8069e526edfd28ceaa29d43a68ef0
TassioSales <74218122+TassioSales@users.noreply.github.com>
2025-05-03 20:46:32 -0300

    Update soma_digitos.py

--- Python Completo do Zero ao Avançado + Projetos Reais 2025/soma_digitos.py
+++ Python Completo do Zero ao Avançado + Projetos Reais 2025/soma_digitos.py
@@ -1,16 +1,6 @@
-# Programa para calcular a soma dos dígitos de um número
-
-# Recebe um número inteiro
-numero = int(input("Digite um número inteiro: "))
-
-# Inicializa a soma
+n = int(input("Digite um número inteiro: "))
 soma = 0
-
-# Loop while para somar os dígitos
-while numero > 0:
-    digito = numero % 10  # Pega o último dígito
-    soma += digito  # Adiciona o dígito à soma
-    numero = numero // 10  # Remove o último dígito
-
-# Imprime o resultado
-print(f"A soma dos dígitos é: {soma}")
+while n > 0:
+    soma += n % 10
+    n //= 10
+print(soma)
