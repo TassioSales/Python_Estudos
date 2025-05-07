4dcfcd3db0607e67d61f8f8dbff0a9b3e78c98ac
TassioSales <74218122+TassioSales@users.noreply.github.com>
2025-05-07 10:01:17 -0300

    Update numeros_impares.py

--- Python Completo do Zero ao Avançado + Projetos Reais 2025/numeros_impares.py
+++ Python Completo do Zero ao Avançado + Projetos Reais 2025/numeros_impares.py
@@ -1,21 +1,6 @@
-# Programa para imprimir os n primeiros números ímpares naturais usando while
-
-# Recebe um número inteiro positivo
-n = int(input("Digite um número inteiro positivo: "))
-
-# Inicializa as variáveis
-numero = 1  # Começa com o primeiro número ímpar
-contador = 0  # Contador de números ímpares impressos
-
-# Loop while para imprimir os n primeiros números ímpares
-while contador < n:
-    print(numero)
-    numero += 2  # Pula para o próximo número ímpar
-    contador += 1  # Incrementa o contador
-
-# Exemplo de saída para n = 5:
-# 1
-# 3
-# 5
-# 7
-# 9
+n = int(input("Digite o valor de n: "))
+if n <= 0:
+    print("Número inválido")
+else:
+    for i in range(1, 2*n, 2):
+        print(i)
