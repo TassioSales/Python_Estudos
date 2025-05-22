c3f8187deee2e180a5f3597295c47807a85891be
TassioSales <74218122+TassioSales@users.noreply.github.com>
2025-05-22 19:20:48 -0300

    Update teste.py

--- Python Completo do Zero ao Avançado + Projetos Reais 2025/teste.py
+++ Python Completo do Zero ao Avançado + Projetos Reais 2025/teste.py
@@ -1,4 +1,19 @@
-i = 1
-while i < n:
-      "iteração"
-      i = i + 1
+def leitura():
+    x = -1
+    while x > 0:
+        x = int(input("Digite um valor: "))
+    return x 
+
+print(leitura())
+
+
+# o que a funcao leitura faz?
+# a funcao leitura le um valor do usuario e retorna ele
+
+# entao pq o resultado e -1?
+# porque o while e while x > 0, ou seja, enquanto x for maior que 0, ele vai continuar lendo
+# e como o valor e -1, ele nao entra no while
+# logo, ele retorna -1
+
+
+
