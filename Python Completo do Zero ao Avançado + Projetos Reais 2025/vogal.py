0b3aeafac4554b1e2cfcc039435977b38fcfd3a8
TassioSales <74218122+TassioSales@users.noreply.github.com>
2025-05-22 19:20:50 -0300

    Create vogal.py

---
+++ Python Completo do Zero ao Avançado + Projetos Reais 2025/vogal.py
@@ -0,0 +1,6 @@
+def vogal(caractere):
+    """
+    Recebe um único caractere como parâmetro e devolve True se ele for uma vogal e False se for uma consoante.
+    """
+    vogais = 'aeiouAEIOU'
+    return caractere in vogais
