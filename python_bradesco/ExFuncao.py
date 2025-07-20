b849710db8fe667dd725fc80ce4f85b9f1812acf
TassioSales <74218122+TassioSales@users.noreply.github.com>
2025-07-19 22:35:29 -0300

    Create ExFuncao.py

---
+++ python_bradesco/ExFuncao.py
@@ -0,0 +1,13 @@
+def mensagem():
+    print("Hello World")
+
+def mensagem2():
+    return "Hello World"
+
+def main():
+    mensagem()
+    print(mensagem2())
+
+
+if __name__ == '__main__':
+    main()
